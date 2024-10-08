f4483d12b53db8198cb1ed1fec55ad7335b90fd3
Martin Madsen <mj@factbird.com>
2024-10-08 13:18:47 +0200

    Cleanup configuration and add back shortcat and yabai

--- flake.nix
+++ flake.nix
@@ -12,38 +12,6 @@
 
   outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
     let
-      # oldPkgs = import oldNixpkgs {
-      #   system = "x86_64-darwin";
-      #   config = {
-      #     allowUnfree = true;
-      #     allowBroken = true;
-      #     allowUnfreePredicate = (_: true);
-      #     allowUnsupportedSystem = true;
-      #   };
-      #   overlays = [(final: prev: {
-      #     # yabai = prev.yabai.overrideAttrs (old: rec {
-      #     #   version = "7.0.2";
-      #     #   src = final.fetchFromGitHub {
-      #     #     owner = "koekeishiya";
-      #     #     repo = "yabai";
-      #     #     rev = "v${version}";
-      #     #     hash = "sha256-/MOAKsY7MlRWdvUQwHeITTeGJbCUdX7blZZAl2zXuic=";
-      #     #   };
-      #     # });
-      #     # bruno = prev.bruno.overrideAttrs (old: rec {
-      #     #   version = "1.11.0";
-      #     #   buildInputs = old.buildInputs ++ [
-      #     #     prev.giflib
-      #     #   ];
-      #     #   src = final.fetchFromGitHub {
-      #     #     owner = "usebruno";
-      #     #     repo = "bruno";
-      #     #     rev = "v${version}";
-      #     #     hash = "sha256-Urskhzs00OEucoR17NDXNtnrcXk9h75E806Re0HvYyw=";
-      #     #   };
-      #     # });
-      #   })];
-      # };
       configuration = { pkgs, ... }: {
         environment.systemPackages =
           with pkgs; [
@@ -63,6 +31,8 @@
             karabiner-elements # Key remapping
             rust-analyzer
             yt-dlp
+            jdk
+            # wine64
             nix-tree
             (ffmpeg.override {
               withWebp = true;
@@ -76,18 +46,25 @@
             qemu
             git-lfs
             darwin.apple_sdk.sdk
-            # rustdesk-flutter
           ];
 
         services.nix-daemon.enable = true;
-        # services.yabai.enable = true;
+        services.yabai = {
+          enable = true;
+          config = {
+            layout = "bsp";
+          };
+          extraConfig = ''
+            yabai -m rule --add app="System Settings" manage=off
+          '';
+        };
 
-        # launchd.user.agents.shortcat = {
-        #   serviceConfig.ProgramArguments = [ "${pkgs.shortcat}/Applications/Shortcat.app/Contents/MacOS/Shortcat" ];
+        launchd.user.agents.shortcat = {
+          serviceConfig.ProgramArguments = [ "${pkgs.shortcat}/Applications/Shortcat.app/Contents/MacOS/Shortcat" ];
 
-        #   serviceConfig.KeepAlive = true;
-        #   serviceConfig.RunAtLoad = true;
-        # };
+          serviceConfig.KeepAlive = true;
+          serviceConfig.RunAtLoad = true;
+        };
 
         nix.settings.experimental-features = "nix-command flakes";
 
@@ -131,44 +108,6 @@
           allowUnsupportedSystem = true;
         };
         overlays = [(final: prev: {
-          # rustdesk-flutter = prev.rustdesk-flutter.overrideAttrs (old: rec {
-          #   buildInputs = [
-          #     prev.fuse3
-          #     prev.gst_all_1.gst-plugins-base
-          #     prev.gst_all_1.gstreamer
-          #     prev.libXtst
-          #     prev.libaom
-          #     prev.libopus
-          #     prev.libpulseaudio
-          #     prev.libva
-          #     prev.libvdpau
-          #     prev.libvpx
-          #     prev.libxkbcommon
-          #     prev.libyuv
-          #     prev.pam
-          #   ];
-          # });
-          # yabai = prev.yabai.overrideAttrs (old: rec {
-          #   version = "7.0.2";
-          #   src = final.fetchFromGitHub {
-          #     owner = "koekeishiya";
-          #     repo = "yabai";
-          #     rev = "v${version}";
-          #     hash = "sha256-/MOAKsY7MlRWdvUQwHeITTeGJbCUdX7blZZAl2zXuic=";
-          #   };
-          # });
-          # bruno = prev.bruno.overrideAttrs (old: rec {
-          #   version = "1.11.0";
-          #   buildInputs = old.buildInputs ++ [
-          #     prev.giflib
-          #   ];
-          #   src = final.fetchFromGitHub {
-          #     owner = "usebruno";
-          #     repo = "bruno";
-          #     rev = "v${version}";
-          #     hash = "sha256-Urskhzs00OEucoR17NDXNtnrcXk9h75E806Re0HvYyw=";
-          #   };
-          # });
           emacs-macport = prev.emacs-macport.overrideAttrs (o: {
             configureFlags = o.configureFlags ++ [
               "CFLAGS=-DMAC_OS_X_VERSION_MAX_ALLOWED=101201"
@@ -220,6 +159,11 @@
                     enable = true;
                     enableZshIntegration = true;
                     nix-direnv.enable = true;
+                    config = {
+                      global = {
+                        hide_env_diff = true;
+                      };
+                    };
                   };
 
                   programs.zsh = {
@@ -235,85 +179,84 @@
                       wget = "curl -O --retry 999 --retry-max-time 0 -C -";
                     };
                     envExtra = ''
-                    export ZSH_TMUX_AUTOSTART=true
-                    export PATH=$PATH:$HOME/projects/tools/cli/bin
-                    export PATH=$PATH:$HOME/projects/bbctl/target/release
-                    export DIRENV_WARN_TIMEOUT=0
+                      export ZSH_TMUX_AUTOSTART=true
+                      export PATH=$PATH:$HOME/projects/tools/cli/bin
+                      export PATH=$PATH:$HOME/projects/bbctl/target/release
+                      export DIRENV_WARN_TIMEOUT=0
 
-                    bpm_key() {
-                      FILE=$1
-                      BPM_LIMIT=''${2:-180}
-                      if [ ! -f "$FILE" ]; then
-                        echo "File not found"
-                        return;
-                      fi
-                      BPM=$(${pkgs.ffmpeg}/bin/ffmpeg -vn -i "$1" -ar 44100 -ac 1 -f f32le pipe:1 2>/dev/null | ${pkgs.bpm-tools}/bin/bpm -x $BPM_LIMIT -f "%03.0f")
-                      KEY=$(${pkgs.keyfinder-cli}/bin/keyfinder-cli $FILE)
-                      echo "''${BPM}_''${KEY}"
-                    }
+                      bpm_key() {
+                        FILE=$1
+                        BPM_LIMIT=''${2:-180}
+                        if [ ! -f "$FILE" ]; then
+                          echo "File not found"
+                          return;
+                        fi
+                        BPM=$(${pkgs.ffmpeg}/bin/ffmpeg -vn -i "$1" -ar 44100 -ac 1 -f f32le pipe:1 2>/dev/null | ${pkgs.bpm-tools}/bin/bpm -x $BPM_LIMIT -f "%03.0f")
+                        KEY=$(${pkgs.keyfinder-cli}/bin/keyfinder-cli $FILE)
+                        echo "''${BPM}_''${KEY}"
+                      }
 
-                    c () {
-                      (assume -c -r eu-west-1 "$1")
-                    }
+                      c () {
+                        (assume -c -r eu-west-1 "$1")
+                      }
 
-                    a () {
-                      assume -r eu-west-1 "$1"
-                    }
+                      a () {
+                        assume -r eu-west-1 "$1"
+                      }
 
-                    killPort () {
-                      kill $(lsof -i:$1 | awk '{ print $2 }' | tail -n +2 | xargs)
-                    }
+                      killPort () {
+                        kill $(lsof -i:$1 | awk '{ print $2 }' | tail -n +2 | xargs)
+                      }
 
-                    developmentStats () {
-                       # List of commits
-                       git log -n 100 --oneline --pretty=format:"%<(30)%an%<(20)%ad%x09%s"
+                      developmentStats () {
+                         # List of commits
+                         git log -n 100 --oneline --pretty=format:"%<(30)%an%<(20)%ad%x09%s"
 
-                       # Developer activity
-                       git log -n 100 --oneline --pretty=format:"%<(30)%an%<(20)%ad%x09%s" | sort | awk '{ print $1 }' | uniq -c
+                         # Developer activity
+                         git log -n 100 --oneline --pretty=format:"%<(30)%an%<(20)%ad%x09%s" | sort | awk '{ print $1 }' | uniq -c
 
-                       # Task distribution
-                       git log -n 100 --oneline --pretty=format:"%an,%ad,%s" | awk -F',' '{ print $3 }' | sort | awk -F'(' '{ print $1 }' | uniq -c
-                                                                            }
+                         # Task distribution
+                         git log -n 100 --oneline --pretty=format:"%an,%ad,%s" | awk -F',' '{ print $3 }' | sort | awk -F'(' '{ print $1 }' | uniq -c
+                      }
 
-                    get_accounts_recursive() {
-                      accounts=$(aws organizations list-accounts-for-parent --parent-id "$1" | jq -r '.Accounts[] | .Id')
+                      get_accounts_recursive() {
+                        accounts=$(aws organizations list-accounts-for-parent --parent-id "$1" | jq -r '.Accounts[] | .Id')
 
-                      for ou in $(aws organizations list-organizational-units-for-parent --parent-id "$1" --output text --query 'OrganizationalUnits[][Id]'); do
-                        accounts="$accounts $(get_accounts_recursive "$ou")"
-                      done
+                        for ou in $(aws organizations list-organizational-units-for-parent --parent-id "$1" --output text --query 'OrganizationalUnits[][Id]'); do
+                          accounts="$accounts $(get_accounts_recursive "$ou")"
+                        done
 
-                      echo "$accounts" | xargs
-                    }
+                        echo "$accounts" | xargs
+                      }
 
-                    BLACKBIRD_APPLICATIONS_OU=ou-h5j2-v74x4pj1
-                    DEVELOPER_ACCOUNTS_OU=ou-h5j2-y3cktc2g
+                      BLACKBIRD_APPLICATIONS_OU=ou-h5j2-v74x4pj1
+                      DEVELOPER_ACCOUNTS_OU=ou-h5j2-y3cktc2g
 
-                    clouds () {
-                      CURRENT_ACCOUNT=$(aws sts get-caller-identity | jq -r .Account)
-                      if [ "$CURRENT_ACCOUNT" != "274906834921" ]; then
-                        return;
-                      fi
+                      clouds () {
+                        CURRENT_ACCOUNT=$(aws sts get-caller-identity | jq -r .Account)
+                        if [ "$CURRENT_ACCOUNT" != "274906834921" ]; then
+                          return;
+                        fi
 
-                      echo $(get_accounts_recursive $BLACKBIRD_APPLICATIONS_OU) $(get_accounts_recursive $DEVELOPER_ACCOUNTS_OU)
-                    }
+                        echo $(get_accounts_recursive $BLACKBIRD_APPLICATIONS_OU) $(get_accounts_recursive $DEVELOPER_ACCOUNTS_OU)
+                      }
 
-                    assume_role () {
-                      CREDENTIALS=`aws sts assume-role --role-arn arn:aws:iam::''${1}:role/AWSControlTowerExecution --role-session-name "$USER" --duration-seconds 3600 --output=json`
+                      assume_role () {
+                        CREDENTIALS=`aws sts assume-role --role-arn arn:aws:iam::''${1}:role/AWSControlTowerExecution --role-session-name "$USER" --duration-seconds 3600 --output=json`
 
-                      export AWS_ACCESS_KEY_ID=`echo ''${CREDENTIALS} | jq -r '.Credentials.AccessKeyId'`
-                      export AWS_SECRET_ACCESS_KEY=`echo ''${CREDENTIALS} | jq -r '.Credentials.SecretAccessKey'`
-                      export AWS_SESSION_TOKEN=`echo ''${CREDENTIALS} | jq -r '.Credentials.SessionToken'`
-                      export AWS_EXPIRATION=`echo ''${CREDENTIALS} | jq -r '.Credentials.Expiration'`
+                        export AWS_ACCESS_KEY_ID=`echo ''${CREDENTIALS} | jq -r '.Credentials.AccessKeyId'`
+                        export AWS_SECRET_ACCESS_KEY=`echo ''${CREDENTIALS} | jq -r '.Credentials.SecretAccessKey'`
+                        export AWS_SESSION_TOKEN=`echo ''${CREDENTIALS} | jq -r '.Credentials.SessionToken'`
+                        export AWS_EXPIRATION=`echo ''${CREDENTIALS} | jq -r '.Credentials.Expiration'`
 
-                      echo "» Changed context to $cloud ($AWS_ACCESS_KEY_ID)."
-                    }
+                        echo "» Changed context to $cloud ($AWS_ACCESS_KEY_ID)."
+                      }
 
+                      replace () {
+                        ${pkgs.ripgrep}/bin/rg $1 --files-with-matches | ${pkgs.xargs} ${pkgs.sed} -i "s/$1/$2/g"
+                      }
+                    '';
 
-                  '';
-                    #                     replace () {
-                    #   ${pkgs.ripgrep}/bin/rg $1 --files-with-matches | ${pkgs.xargs} ${pkgs.sed} -i "s/$1/$2/g"
-                    # }
-                    # rg --files | grep 'package.json$' | xargs rg --files-with-matches cdk-aws | xargs rg --files-without-match zod
                     oh-my-zsh = {
                       enable = true;
                       plugins = [
@@ -404,7 +347,10 @@
                       cursor_shape = "block";
                     };
                   };
-                  # Disabled due to Swift failing to build
+
+                  # Disabled due to Swift failing to build -> awaiting
+                  # progression on
+                  # https://github.com/NixOS/nixpkgs/issues/344920#issuecomment-2379762180
                   # programs.mpv = {
                   #   enable = true;
                   # };
@@ -416,11 +362,9 @@
                   programs.vscode = {
                     enable = true;
                     extensions = with pkgs.vscode-extensions; [
-                      # ntbbloodbath.doom-one
                       tuttieee.emacs-mcx
                       tiehuis.zig
                       rust-lang.rust-analyzer
-                      # ms-vsliveshare.vsliveshare
                       kahole.magit
                       graphql.vscode-graphql
                     ];
