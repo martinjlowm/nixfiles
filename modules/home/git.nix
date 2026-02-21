faa4d717508e739be4325e024674f0c6b8552cc7
Martin Jesper Low Madsen <mj@factbird.com>
2026-02-21 17:03:01 -0600

    Add SSH commit signing

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,11 +1,20 @@
 # Git configuration
-_: {
+{
+  pkgs,
+  nextPkgs,
+  ...
+}: let
+  allowedSignersContent = pkgs.writeText "allowed_signers" ''
+    martinjlowm@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILeNFTkHtufnDHSUoT1D3gfIKbpJjOTd1CQoBSrRYcz8
+  '';
+in {
   programs.git = {
     enable = true;
     settings = {
       user = {
         name = "Martin Jesper Low Madsen";
         email = "mj@factbird.com";
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILeNFTkHtufnDHSUoT1D3gfIKbpJjOTd1CQoBSrRYcz8";
       };
       alias = {
         stashgrep = ''!f() { for i in `git stash list --format="%gd"`; do git stash show -p $i | grep -H --label="$i" "$@"; done; }; f'';
@@ -18,6 +27,16 @@ _: {
       };
       pull = {
         rebase = false;
+      };
+      commit = {
+        gpgsign = true;
+      };
+      gpg = {
+        format = "ssh";
+        ssh = {
+          program = "${nextPkgs._1password-gui}/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+          allowedSignersFile = "${allowedSignersContent}";
+        };
       };
     };
   };
